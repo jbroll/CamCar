bf904da32e241f93dc23a18d5ffd2667ff3bc35a
John Roll <john@rkroll.com>
2025-02-15 11:57:18 -0500

    Split index.html

--- src/gen/file-entries.cpp
+++ src/gen/file-entries.cpp
@@ -1,6 +1,8 @@
 #include "../FileSystem.h"
 
 // External references to File structures defined in individual .cpp files
+extern const FileEntry Camera_js_file;
+extern const FileEntry TankDrive_js_file;
 extern const FileEntry config_html_file;
 extern const FileEntry error_log_file;
 extern const FileEntry index_html_file;
@@ -9,6 +11,8 @@ extern const FileEntry style_css_file;
 
 // Array of pointers to all files
 const FileEntry* const FileSystem::files[] = {
+    &Camera_js_file,
+    &TankDrive_js_file,
     &config_html_file,
     &error_log_file,
     &index_html_file,
