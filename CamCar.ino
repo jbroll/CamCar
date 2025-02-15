ae0fe7f57be5d0e3888fe04e052541bfcc589b36
John Roll <john@rkroll.com>
2025-02-14 21:02:51 -0500

    Camera.cpp

--- CamCar.ino
+++ CamCar.ino
@@ -9,6 +9,8 @@
 
 #include "./Prefs.h"
 
+#include "src/Camera.h"
+
 #include "src/PrefEdit.h"
 #include "src/WebHandler.h"
 #include "src/TankDrive.h"
@@ -48,32 +50,12 @@ const int PWMResolution = 8;
 const int PWMSpeedChannel = 2;
 const int PWMLightChannel = 3;
 
-//Camera related constants
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
-
-const boolean AP = false;
-const char* ssid = "lucky7";
-const char* password = "snowblower";
-
 AsyncWebServer server(80);
 AsyncWebSocket wsCamera("/Camera");
 AsyncWebSocket wsCarInput("/CarInput");
 uint32_t cameraClientId = 0;
+
+CameraHandler camera(wsCamera);
 
 void rotateMotor(int motorNumber, int motorDirection) {
   if (motorDirection == FORWARD) {
@@ -174,98 +156,28 @@ void onCarInputWebSocketEvent(AsyncWebSocket *server,
   }
 }
 
-void onCameraWebSocketEvent(AsyncWebSocket *server, 
-                      AsyncWebSocketClient *client, 
-                      AwsEventType type,
-                      void *arg, 
-                      uint8_t *data, 
-                      size_t len) {                      
-  switch (type) {
-    case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      cameraClientId = client->id();
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      cameraClientId = 0;
-      break;
-    case WS_EVT_DATA:
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      break;
-    default:
-      break;  
-  }
-}
-
-void setupCamera() {
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_4;
-  config.ledc_timer = LEDC_TIMER_2;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  
-  config.frame_size = FRAMESIZE_VGA;
-  config.jpeg_quality = 10;
-  config.fb_count = 1;
-
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }  
-
-  if (psramFound()) {
-    heap_caps_malloc_extmem_enable(20000);  
-    Serial.printf("PSRAM initialized. malloc to take memory from psram above this size");    
-  }  
-}
-
-void sendCameraPicture() {
-  if (cameraClientId == 0) {
-    return;
-  }
-  unsigned long  startTime1 = millis();
-  //capture a frame
-  camera_fb_t * fb = esp_camera_fb_get();
-  if (!fb) {
-      Serial.println("Frame buffer could not be acquired");
-      return;
-  }
-
-  unsigned long  startTime2 = millis();
-  wsCamera.binary(cameraClientId, fb->buf, fb->len);
-  esp_camera_fb_return(fb);
-    
-  //Wait for message to be delivered
-  while (true) {
-    AsyncWebSocketClient * clientPointer = wsCamera.client(cameraClientId);
-    if (!clientPointer || !(clientPointer->queueIsFull())) {
-      break;
+void onCameraWebSocketEvent(AsyncWebSocket *server,
+                          AsyncWebSocketClient *client,
+                          AwsEventType type,
+                          void *arg,
+                          uint8_t *data,
+                          size_t len) {
+    switch (type) {
+        case WS_EVT_CONNECT:
+            Serial.printf("WebSocket client #%u connected from %s\n",
+                         client->id(), client->remoteIP().toString().c_str());
+            camera.setClientId(client->id());
+            break;
+        case WS_EVT_DISCONNECT:
+            Serial.printf("WebSocket client #%u disconnected\n", client->id());
+            camera.clearClientId();
+            break;
+        case WS_EVT_DATA:
+        case WS_EVT_PONG:
+        case WS_EVT_ERROR:
+        default:
+            break;
     }
-    delay(1);
-  }
-  
-  unsigned long  startTime3 = millis();  
-  Serial.printf("Time taken Total: %d|%d|%d\n",startTime3 - startTime1, startTime2 - startTime1, startTime3-startTime2 );
 }
 
 void setUpPinModes() {
@@ -305,9 +217,13 @@ void setup(void) {
   Serial.printf("Free Heap: %d bytes\n", ESP.getFreeHeap());
   Serial.printf("Flash Size: %d bytes\n", ESP.getFlashChipSize());
 
-  WiFi.setTxPower(WIFI_POWER_19_5dBm);
+  const boolean AP = true;
+#define WIFI_CONNECT_DELAY 500
 
   if ( AP ) {
+      const char* ssid = "CamCar";
+      const char* password = "1234567890";
+
       if ( !WiFi.softAP(ssid, password)) {
           Serial.println("Failed to create WiFi access point!");
       }
@@ -319,14 +235,17 @@ void setup(void) {
       Serial.printf("AP IP address:");
       Serial.println(WiFi.softAPIP());
   } else {
+      const char* ssid = "lucky7";
+      const char* password = "snowblower";
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     Serial.println("\nConnecting to WiFi Network ..");
 
     while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(250);
+      digitalWrite(STATUS_LED, !digitalRead(STATUS_LED));  // Toggle LED
+      Serial.print(".");
+      delay(WIFI_CONNECT_DELAY);
     }
 
     Serial.printf("\nConnected to the WiFi network at ");
@@ -345,7 +264,10 @@ void setup(void) {
   server.begin();
   Serial.println("HTTP server started");
 
-  setupCamera();
+  if (!camera.begin()) {
+    Serial.println("Camera initialization failed!");
+    return;
+  }
 }
 
 void loop() {
@@ -359,5 +281,6 @@ void loop() {
 
   wsCamera.cleanupClients(); 
   wsCarInput.cleanupClients(); 
-  sendCameraPicture(); 
+
+  camera.sendFrame();
 }
